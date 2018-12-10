11ad28512555eeeb034fe674371b067a1d41266a
Benard Kong <benardkong@hotmail.com>
2018-12-10 12:33:35 +0900

    Completed Days 1-4

---
+++ Day02.py
@@ -0,0 +1,327 @@
+input_str = """icxjubroqtunzeyzpomfksahgw
+ibxjvbroqtunleyzjdmfksahow
+icxjvbroqtinleyzpdmflsahnw
+icxjvbnoqtunleyvpgmfksahgw
+wcxjvbroqrunleyzpdmfksahge
+icxjtbroqtjzleyzpdmfksahgw
+icxjvbrohtunleyzpdmfkbahsw
+xcxjvbroqcunleyzpdmfksaxgw
+ycxjvbroqtunleyzpowfksahgw
+icxfvbroqtunleyzpdmfksncgw
+ixxjvbuoqtunleyzpdvfksahgw
+icfjvbroqtunleyzpdmfksadgt
+icxjvbroqdunleyzpdafksqhgw
+icxjvbrobtunlelzpdmfkuahgs
+ujxjvbroqtunleyzpdmqksahgw
+icqjvsroqtunleyzpdmfksahuw
+icxjvbroptpnleyzpdmfksangw
+ipxjvbroqtunleyzpdmfesahgi
+icajvbroqtunltyzpdqfksahgw
+ickjvbroqtuzleyzpdmfgsahgw
+icxjvbroqtunledzpdmwksahgz
+icxjvlroqtsnleyzpdmfksvhgw
+icxjvbroqtunleyzpdsfysahvw
+icxjvbroqtunwnyzydmfksahgw
+ionjvbroqtunleyzpdmfksahgj
+icxjvwriqtunleyzpdmfksahgi
+ocxjvbroztunleyzpdmfksapgw
+icxjvbroqtmnlewzpumfksahgw
+ucxjvbroqtunleyzpdmzktahgw
+icxgvbroqtunleyztdmfktahgw
+icxhvbroqttnleybpdmfksahgw
+icxjvbroqtugleyzpdxfkeahgw
+acxjvbroqvunlexzpdmfksahgw
+icxjvbroqglnleyzpbmfksahgw
+icxjvbriqtnvleyzpdmfksahgw
+icxjvbreqtunlakzpdmfksahgw
+gcxjvbuoqtunleyzpdmfksawgw
+icxjvbroqtunleyzpddfkyzhgw
+icxjvbjoqtunleyzpdmfqsahhw
+icxjvjroqtunleyzpnmfksajgw
+ycxjvbroqtunmeyzcdmfksahgw
+irxkvbryqtunleyzpdmfksahgw
+isxjvbrlqtunleyzpdmsksahgw
+icxjvbcoqtunleyzpdfkksahgw
+ixnjvbroqtunleyzpdmfkqahgw
+wcxjvbroqhunleyzqdmfksahgw
+icljvurmqtunleyzpdmfksahgw
+ibxjvbroqtayleyzpdmfksahgw
+arxjvbroqiunleyzpdmfksahgw
+iuxjvbroqtunluyzpdmoksahgw
+icxjvbrmqtunleyzpdmfosahew
+isxjvbroqtunleyrpdmfksrhgw
+icxjvxrpqtunleyzpdmfkdahgw
+ichjvbrogtunllyzpdmfksahgw
+icxjvbeoqtunlryzpdmfksakgw
+icxjvbroqtcnemyzpdmfksahgw
+icxjvbroqtybledzpdmfksahgw
+icxjvbrqqtunleyzpdmfksgngw
+icgjvbroqtunleyzmdmfksabgw
+icxjtbroqtunleyzkdmfksahww
+icxjvbfoqtunleyzpamfqsahgw
+icxjvbroknuyleyzpdmfksahgw
+icxjvbroqtujleyzpdmaksaigw
+icxjvbroqtnnlmyzpdmflsahgw
+icxjvbroqtunlefzpdmfsfahgw
+icxjvdroqtusleyzpdzfksahgw
+icxjvbioqtunlsyzpdmfkshhgw
+icxbvbrodtunleyzpdmoksahgw
+icxjvbroqtuvleyzpdmfkbahmw
+iyxjvbroqtunljvzpdmfksahgw
+icxjvbroqtudleynddmfksahgw
+icxjvwroqtnnleyzpdmfksahgz
+ichjvbroqtunleyzjdmeksahgw
+icxjvbrostunluyrpdmfksahgw
+icfjvbroqtunleyxpdgfksahgw
+nhxjvbroqtunlerzpdmfksahgw
+icxjvbrothunlexzpdmfksahgw
+icxjvbrzltqnleyzpdmfksahgw
+icxjvbrhqtunleyzpdmfksajgy
+vcxjvjroqiunleyzpdmfksahgw
+icxjfbroltunleyzpdmqksahgw
+icxbvbroqtunleyzpdofasahgw
+icxjvbkoqtunveyzpdmfksaqgw
+icxsebroqtunleyzpdmuksahgw
+icxjvbroquunlpyrpdmfksahgw
+icxhvbroqtunjeyzpdmrksahgw
+icdjvbroqtunlzyzpdmfksangw
+jcxqvbroqtvnleyzpdmfksahgw
+icxjvxroqtunleyrpdmfxsahgw
+icxjvnroqtunleyzpdmfssyhgw
+icxjvbraptunleyzpdofksahgw
+icxjvbroatunleyjpdmfbsahgw
+icxjvbroytlnlryzpdmfksahgw
+iaxjvbroqkunleyzpdmfcsahgw
+ucxjvbroqtuileyzzdmfksahgw
+icxjqbroqtcnleyzpgmfksahgw
+icxjvbloqtunleyzadmfksaqgw
+icxjvbroqtunleyzkdmnksakgw
+icxjvbroqtunleyjpdxfksahvw
+iqxjvbroqtujleyzpdmfklahgw
+icgjvbroqtunleyzpdmfksbhgb
+icxjzbroqtunleyzpdmfkdahgg
+icxjvbrobtunloywpdmfksahgw
+icxavbroqtunleyfpdmfksahgd
+icxjvbroqtunleyophmfksahkw
+icxjndroqtunlyyzpdmfksahgw
+icxjvbroqtjnleyppdmvksahgw
+icxjvbroonfnleyzpdmfksahgw
+icxjvbrqqtlnljyzpdmfksahgw
+icxjvbrzqtunlelspdmfksahgw
+icxjvbooqtunleyztdmfkfahgw
+icajvbroltunlnyzpdmfksahgw
+icxjvbroqtunleyzidmdkschgw
+icxjvbroktupleyzpdmfksahyw
+icxjcbroyeunleyzpdmfksahgw
+icxjvbroqtunlezkpdmfksahsw
+icxjvbroqtunlejzpcmfksrhgw
+icxjvvroqtunlsyzkdmfksahgw
+icxjnbroqtunbeyzpdmfpsahgw
+itxjbbroqtunleyzpemfksahgw
+icxjvbroqtunlrkzpdmfksshgw
+rcxjvbroqtujlbyzpdmfksahgw
+icxjvmroqtugleazpdmfksahgw
+icxjvbfoqtunleylpdmfkeahgw
+icnjvoroktunleyzpdmfksahgw
+icxjvbroqtunlvyzpdmfkfahgr
+icxjvbroqtgnseyzpdmfxsahgw
+scxjvbroqtunleycpdmfksdhgw
+icxhvbxoqtunleuzpdmfksahgw
+icxjvbruqtunleyzpnmfksfhgw
+icdjvbroqtunleyzpdmfksahup
+ihxjvbroqtunleovpdmfksahgw
+icxjvbroqtunleyzxdmfksahzv
+ocxjvbioqtunleyzpdmfzsahgw
+idxjvbroqtunlyyzpdofksahgw
+izdjvbroqtunleyzpdtfksahgw
+icxjvbrnqtunleyzpdmfksbhgb
+icxjvbrjqtunleyhpdmrksahgw
+icxjvbroqtunleyzpdbflsahgg
+icxjvbmfqtunleyzpdmfkaahgw
+idxjvbroqtunlsyzpdffksahgw
+bcxjvbroqtunleyzpkmfkswhgw
+ivxjvbroqtdnleyzpdmbksahgw
+icxpvbboqtunleyzpdmfksahtw
+ibxjvbroqtunlehzpdmfkmahgw
+icxjvbboqtunleyzpdmfkaahgv
+icxjlaroqtuileyzpdmfksahgw
+icxjvbroftunleyzpdmfqsahew
+ichjvbroqtunleyzpdmiwsahgw
+icxrvbvoqtunleyzpdmiksahgw
+icxjvbroqtunldydpdmfksahgl
+icogvbroqtunleyzpdmfnsahgw
+icxjvbroqtunleszodmfkswhgw
+icxjvbrontunleyztemfksahgw
+icxjvbrovtunleyzpdvkksahgw
+icxjvbroqqucteyzpdmfksahgw
+icmovbroptunleyzpdmfksahgw
+icxjvbqoftunleyzvdmfksahgw
+icxjvbdoqtunleyzpdmfkadhgw
+icxjvbroqtunlgnzpdmfksaqgw
+icxjvbroqtunieygpdyfksahgw
+acdjvbroqtunleyzpdmfkwahgw
+icxjvbroqtunleyzpdmfkfahlj
+icxjvbgoqtunleyepdmvksahgw
+icxjvbpobbunleyzpdmfksahgw
+icxjvbroqtunleurpdmfktahgw
+ipxjvbzoqtunleyzpdmffsahgw
+icxjtbroqtunllyzpdmuksahgw
+icxjvbroqtunbsyzadmfksahgw
+ihxjvoroqtuqleyzpdmfksahgw
+idxjmbroqqunleyzpdmfksahgw
+wcxjvbdoqtunleyzpdmfksahgr
+icxjvbroqtunleygptmfksahgj
+ipxjvbrsqtunleyzpdmfksghgw
+ycxjvbroqtunluyzkdmfksahgw
+icxjvbroxtuulejzpdmfksahgw
+icqjvbroqtunlwyypdmfksahgw
+ioxjhbroqtunleyzphmfksahgw
+icxjvbgoqnunleyzpdmfksahaw
+mcxjvbroqtunleyzpdmfksihgh
+icxjsbroqtunlqyzpdmfksawgw
+icxjvbroqtuoleycpdmftsahgw
+icxjvbroqtunleyzgdifksahlw
+icxjvbmoqtunleyzjfmfksahgw
+icxjvbroqtunlezopdmfksahge
+icxjvbroqtbnlefzpdmfosahgw
+tcxjvbromtunlevzpdmfksahgw
+irxjgbroqtunleyzpdmfksthgw
+icxjvbrojtunleyxpdmoksahgw
+icxrvbroytpnleyzpdmfksahgw
+icxjvbroqtunfeyupdmfksasgw
+ihqjvbroqtunleyzpdmftsahgw
+icxjobroqkunleozpdmfksahgw
+icjjjbroqtualeyzpdmfksahgw
+icxjvbroqtunaeytpdmfksahvw
+icxjvbroqtunzeyzpdmfkshhxw
+icxqvbroqtucleyzxdmfksahgw
+icxjvbrogturleyzxdmfksahgw
+icxjvoqoqtunleyzpdcfksahgw
+iuxjvbroqtunleyzpdmfksopgw
+icxjveroqtunleyzptmfksalgw
+icxjvbroqtunleyzpdmfwcahhw
+iwxjvbroqtlnleyzpdmfksyhgw
+ectjvbroqtanleyzpdmfksahgw
+icxjvnroqtyhleyzpdmfksahgw
+icvjvhboqtunleyzpdmfksahgw
+icxjtbroqtuzleyupdmfksahgw
+icjjvproqtunleyzpsmfksahgw
+icdjvbroqtutleyzpdmiksahgw
+icxjvwroqtujleyzpdmfksahgc
+icxjxbroqtunleyzpdwhksahgw
+icxjvbqoqtunleyzpdmvfsahgw
+icajvbroqtusleyzpdmfksaagw
+icxjvbroqtunbtyzpdmfksmhgw
+kcxjvbroqtxnleyzpdmfkskhgw
+icxjvbqogfunleyzpdmfksahgw
+icxjvbroqtubleyzpdmfdswhgw
+icxjvprlqtunleyzpdmffsahgw
+icxjxbroqtucleyzpdmfksakgw
+dcxrvbroqtunleycpdmfksahgw
+icxjvbrobtunleyzpomfksahgu
+ocxrvbroqtunleyzpdmfssahgw
+icxjvbroktunlejzpdmfksahzw
+icxjvbrovtunleyzmdmfkhahgw
+icxjvbroqtudleygpdmfksfhgw
+bcxjvbroqtubllyzpdmfksahgw
+icxwvbrontunzeyzpdmfksahgw
+icxjvbroqtunleysjbmfksahgw
+icxjvvroztunleyzpdmfksjhgw
+ivxjxbroqtunleyzpdmfksahew
+icxjvbroqtunleyupqufksahgw
+icxjvmrcqtunleyzpdmxksahgw
+icxjvgroqtunleyzpdgfkuahgw
+icxjvbroqthnqeyfpdmfksahgw
+icxjsbuodtunleyzpdmfksahgw
+iuxjzbroqtunleyzpdrfksahgw
+icxjvbrobtunlelzpdmfksahgs
+icxjvbroqtzhljyzpdmfksahgw
+inxtvbroqtunleyzpdmeksahgw
+icgjvbroqtunleyztdmfksahgq
+icxjvagoqtugleyzpdmfksahgw
+icxuvbroqtunleyzpimfkyahgw
+icxzvbroqtfhleyzpdmfksahgw
+icxjjbroqtqnleyzpdmnksahgw
+icjrvbroqtunleszpdmfksahgw
+iexjvbroqtunlgyzpdmfksacgw
+rcxjvbkoqtuoleyzpdmfksahgw
+icxjvbroqgunlwyzpdmfksqhgw
+icxjvbroqtunleqzpsmfksqhgw
+icxjvbroqtubaeyzpdmfksaugw"""
+
+def find_duplicates(box_id):
+    # Returns a (bool, bool) tuple representing whether there are exactly
+    # 2 and 3 of a letter
+    let_dict = {}
+    two = False
+    three = False
+    for letter in box_id:
+        try:
+            let_dict[letter] += 1
+        except:
+            let_dict[letter] = 1
+    for k, v in let_dict.items():
+        if v == 2:
+            two = True
+        elif v == 3:
+            three = True
+    return (two, three)
+
+def count_checksum(s):
+    two = 0
+    three = 0
+    s = s.split("\n")
+    for box in s:
+        dup_result = find_duplicates(box)
+        if dup_result[0]:
+            two += 1
+        if dup_result[1]:
+            three += 1
+    return two * three
+
+## Part 2
+def off_by_one(s, t):
+    # Compare the strings s and t
+    # Returns (bool, int) tuple
+    # For bool, Returns True if s and t are only off by 1 letter AND the "off" letter
+    # is in the same index
+    # For int, returns the index (if True)
+    # If False, returns index of -1
+
+    if s == t:
+        return (False, -1)
+    else:
+        assert len(s) == len(t), "'%s' and '%s' do not have the same number of letters" % (s, t)
+        i = 0
+        length = len(s)
+        index, times_not_matched = 0, 0
+        while i < length:
+            if s[i] == t[i]:
+                pass
+            else:
+                index = i
+                times_not_matched += 1
+                if times_not_matched > 1:
+                    return (False, -1)
+            i += 1
+        return (True, index)
+
+def check_all_combos(s):
+    #s: a string of Box IDs separated by \n
+    #Returns two strings that are only off by 1
+    s = s.split("\n")
+    i = 0
+    length = len(s)
+    while i < length - 1:
+        j = i + 1
+        while j < length:
+            off_by_one_var, index = off_by_one(s[i], s[j])
+            if off_by_one_var:
+                #return s[i] # Incorrect, you need the COMMON letters among the two
+                return s[i][:index] + s[i][index + 1:]
+            j += 1
+        i += 1
+
+if __name__ == "__main__":
+    #print(count_checksum(input_str))
+    print(check_all_combos(input_str))
---
+++ Day04.py
@@ -0,0 +1,16 @@
+
+
+## Day 4
+
+import datetime
+## Use datetime.datetime.strptime(date_string, format) method
+## to parse your string into a datetime object.
+## eg. for use: datetime.datetime.strptime("1518-11-01 23:58", "%Y-%m-%d %H:%M")
+## Then, it will be possible to organize the datetime objects in a list
+## by using comparison <, >, or == just as with integers.
+## eg. datetime.datetime.strptime("1518-11-01 23:58", "%Y-%m-%d %H:%M")
+## <
+## datetime.datetime.strptime("1518-11-02 00:00", "%Y-%m-%d %H:%M")
+## -> True
+## Source: https://docs.python.org/3/library/datetime.html
+
